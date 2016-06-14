2d78aad965824a18c0002c7d9b97183b665f3aca
JP Labadie <jlabadie@tgen.org>
2016-06-14 14:07:24 -0700

    Create ImageButton wrapper to allow icons as buttons

--- src/main/java/widgets/ImageButton.java
+++ src/main/java/widgets/ImageButton.java
@@ -1,10 +1,40 @@
 package widgets;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 /**
  * Project naspgui.
  * Created by jlabadie on 6/14/16.
  *
- * @Author jlabadie
+ * @author jlabadie
  */
-public class ImageButton {
+public class ImageButton extends Button{
+
+        private final String STYLE_NORMAL = "-fx-background-color: transparent; -fx-padding: 5, 5, 5, 5;";
+        private final String STYLE_PRESSED = "-fx-background-color: transparent; -fx-padding: 6 4 4 6;";
+
+        public ImageButton(String imageurl) {
+            setGraphic(new ImageView(new Image(getClass().getResourceAsStream(imageurl))));
+            setStyle(STYLE_NORMAL);
+
+            setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    setStyle(STYLE_PRESSED);
+                }
+            });
+
+            setOnMouseReleased(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    setStyle(STYLE_NORMAL);
+                }
+            });
+        }
+
+
 }
