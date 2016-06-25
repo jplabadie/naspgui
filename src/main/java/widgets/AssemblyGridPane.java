58e8f697037a51a6119a11585181bee39663ac3f
JP Labadie <jlabadie@tgen.org>
2016-06-24 17:16:51 -0700

    Add GridPane objects, tests, and css for the Job window

---
+++ src/main/java/widgets/AssemblyGridPane.java
@@ -0,0 +1,10 @@
+package widgets;
+
+/**
+ * Project naspgui.
+ * Created by jlabadie on 6/24/16.
+ *
+ * @Author jlabadie
+ */
+public class AssemblyGridPane {
+}
---
+++ src/main/java/widgets/JobGridPane.java
@@ -0,0 +1,10 @@
+package widgets;
+
+/**
+ * Project naspgui.
+ * Created by jlabadie on 6/16/16.
+ *
+ * @Author jlabadie
+ */
+public class JobGridPane {
+}
