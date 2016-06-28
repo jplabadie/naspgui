1354abb8a8785c4d65be6d6031c5c836c16dac8f
JP Labadie <jlabadie@tgen.org>
2016-06-28 09:43:52 -0700

    Add and refactor AssemblyGridPanes

--- src/main/java/widgets/AssemblyFolderGridPane.java
+++ src/main/java/widgets/AssemblyFolderGridPane.java
@@ -1,10 +1,172 @@
 package widgets;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import java.util.ArrayList;
+
 /**
  * Project naspgui.
- * Created by jlabadie on 6/28/16.
+ * Created by jlabadie on 6/15/16.
  *
- * @Author jlabadie
+ * @author jlabadie
  */
-public class AssemblyFolderGridPane {
+class AssemblyFolderGridPane extends GridPane {
+
+    private GridPane AF = this;
+    private Label ASSEMBLY_FOLDER = new Label( "Assembly Folder" );
+    private Label ASSEMBLY_FOLDER_PATH = new Label( "Folder Path" );
+
+    private TextField assembly_folder_path = new TextField();
+
+    private Tooltip ASSEMBLY_FOLDER_PATH_TIP
+            = new Tooltip( "The remote path containing the assemblies you are interested in" );
+
+    private Image add = new Image( getClass().getResourceAsStream( "/icons/add-3.png" ) );
+    private Image remove = new Image( getClass().getResourceAsStream( "/icons/stop.png" ) );
+
+    private ObservableList<AssemblyGridPane> assembly_gridpanes;
+
+    private int grid_row_position = 2;
+
+
+    AssemblyFolderGridPane(){
+        /**
+         * Initialize the observable list which will hold the read pairs for this widget
+         */
+        ArrayList<AssemblyGridPane> ass_pairings =  new ArrayList<>();
+        assembly_gridpanes = FXCollections.observableList( ass_pairings );
+
+        /**
+         * Define the look and feel of static label elements
+         */
+        ASSEMBLY_FOLDER.setFont( Font.font("Helvetica", FontWeight.EXTRA_BOLD, 18 ) );
+        ASSEMBLY_FOLDER.setPrefSize( 100, 20 );
+        ASSEMBLY_FOLDER.setAlignment( Pos.CENTER );
+        ASSEMBLY_FOLDER.setPrefSize( USE_COMPUTED_SIZE, USE_COMPUTED_SIZE );
+        ASSEMBLY_FOLDER.setAlignment( Pos.CENTER );
+        ASSEMBLY_FOLDER_PATH.setFont( Font.font( "Courier", FontWeight.BOLD, 14 ) );
+
+        /**
+         * Add tooltips to the static label elements
+         */
+        ASSEMBLY_FOLDER_PATH.setTooltip(ASSEMBLY_FOLDER_PATH_TIP);
+
+        /**
+         * Define the look and behavior of the GridPane
+         */
+        // Set Horizontal and Vertical gap size (spacing between column areas)
+        this.setHgap( 4 );
+        this.setVgap( 4 );
+        //Define column behavior (min_size, preferred_size, max_size)
+        ColumnConstraints c0 = new ColumnConstraints( 30, 60, 90 );
+        ColumnConstraints c1 = new ColumnConstraints( 30, 60, 90 );
+        ColumnConstraints c2 = new ColumnConstraints( 30, 60, 90 );
+        ColumnConstraints c3 = new ColumnConstraints( 30, 60, 90 );
+        //Define column auto-resizing behavior
+        c1.setHgrow( Priority.NEVER );
+        c2.setHgrow( Priority.ALWAYS );
+        c3.setHgrow( Priority.SOMETIMES );
+        c3.setHalignment( HPos.RIGHT );
+        // Add column behavior to the GridPane (order matters!)
+        this.getColumnConstraints().addAll( c0, c1, c2, c3  );
+
+        /**
+         * Define the look and behavior of the non-static TextField and Label elements
+         */
+
+        // Add the title to row 0 column 0
+        this.add( ASSEMBLY_FOLDER, 0, 0, 3, 1 );
+
+        // Add row headings for app-path and app-args to column 1
+        this.add( ASSEMBLY_FOLDER_PATH, 1, 1, 3, 1 );
+        this.add( assembly_folder_path, 3, 1, 4, 1 );
+
+        // Add the button to the widget with an event handler
+
+        ImageView image_view = new ImageView( add );
+        image_view.setFitHeight( 20 );
+        image_view.setFitWidth( 20 );
+
+        assembly_gridpanes.addListener(new ListChangeListener<GridPane>() {
+
+            @Override
+            public void onChanged( Change<? extends GridPane> c ) {
+                while ( c.next() ) {
+                    if ( c.wasAdded() ) {
+                        for ( GridPane gp : c.getAddedSubList() ) {
+                            // Add the remove button to the widget
+                            Button remove_assembly = new Button();
+
+                            Button add_assembly = new Button();
+                            ImageView image_view1 = new ImageView( add );
+                            image_view1.setFitHeight( 20 );
+                            image_view1.setFitWidth( 20 );
+                            remove_assembly.setGraphic( image_view1 );
+                            add_assembly.setGraphic( image_view1 );
+                            add_assembly.setAlignment( Pos.BOTTOM_RIGHT );
+
+                            add_assembly.setOnAction( event -> addAssembly() );
+
+                            ImageView image_view2 = new ImageView( remove);
+                            image_view2.setFitHeight( 20 );
+                            image_view2.setFitWidth( 20 );
+                            remove_assembly.setGraphic( image_view2 );
+
+                            HBox hbox = new HBox();
+                            hbox.getChildren().addAll( remove_assembly, add_assembly );
+                            hbox.setAlignment( Pos.BOTTOM_CENTER );
+                            AF.add( hbox, 5, grid_row_position, 3, 1 );
+
+                            remove_assembly.setOnAction(
+                                    event -> {
+                                        if(assembly_gridpanes.size() > 1) {
+                                            assembly_gridpanes.remove( gp );
+                                            AF.getChildren().remove( hbox );
+                                        }
+                                        else if( assembly_gridpanes.size() == 1 ){
+                                            AssemblyGridPane ap = assembly_gridpanes.get( 0 );
+                                            ap.clear();
+                                        }
+                                    }
+                            );
+                            AF.add( gp, 2, grid_row_position++, 3, 1 );
+                        }
+                    }
+                    if ( c.wasRemoved() ) {
+                        for ( GridPane gp : c.getRemoved() ) {
+                            AF.getChildren().remove( gp );
+                            grid_row_position--;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+
+    void addAssembly(String sample_name, String sample_path ){
+        AssemblyGridPane ap = new AssemblyGridPane( sample_name, sample_path );
+        assembly_gridpanes.add( ap );
+    }
+
+    void addAssembly( ){
+        AssemblyGridPane ap = new AssemblyGridPane();
+        assembly_gridpanes.add( ap );
+    }
 }
--- src/main/java/widgets/AssemblyGridPane.java
+++ src/main/java/widgets/AssemblyGridPane.java
@@ -1,39 +1,55 @@
 package widgets;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import xmlbinds.Assembly;
+
+import java.util.ArrayList;
 
 /**
  * Project naspgui.
- * Created by jlabadie on 6/24/16.
+ * Created by jlabadie on 6/16/16.
  *
  * @author jlabadie
  */
-public class AssemblyGridPane extends GridPane {
+class AssemblyGridPane extends JobGridPane {
 
-    private static Label SAMPLE_NAME = new Label( "Sample Name" );
-    private static Label FILE_NAME = new Label( "File Name" );
+    private Label ASSEMBLY = new Label( "Assembly" );
+    private Label ASSEMBLY_NAME = new Label( "Assembly Name" );
+    private Label ASSEMBLY_PATH = new Label( "Assembly Path" );
 
-    private static Tooltip SAMPLE_NAME_TIP = new Tooltip( "The name given to this sample" );
-    private static Tooltip FILE_NAME_TIP = new Tooltip( "The name of the sample file in the folder" );
+    private Tooltip ASSEMBLY_NAME_TIP = new Tooltip( "The name given to this assembly" );
+    private Tooltip ASSEMBLY_PATH_TIP = new Tooltip( "The path (file name) of this assembly" );
 
-    private TextField sample_name = new TextField();
-    private TextField file_name = new TextField();
+    private TextField assembly_name = new TextField();
+    private TextField assembly_path = new TextField();
+
+    private Assembly assembly;
+
+    private ObservableList<TextField> elements;
+
+    AssemblyGridPane( String name, String path){
+        assembly_name.setText( name );
+        assembly_path.setText( path );
+    }
 
     AssemblyGridPane(){
 
-        SAMPLE_NAME.setFont( Font.font( "Courier", FontWeight.BOLD, 14 ) );
-        FILE_NAME.setFont( Font.font( "Courier", FontWeight.BOLD, 14 ) );
+        ASSEMBLY.setFont( Font.font( "Courier", FontWeight.BOLD, 14 ) );
+        ASSEMBLY_NAME.setFont( Font.font( "Courier", FontWeight.BOLD, 14 ) );
+        ASSEMBLY_PATH.setFont( Font.font( "Courier", FontWeight.BOLD, 14 ) );
 
-        SAMPLE_NAME.setTooltip(SAMPLE_NAME_TIP);
-        FILE_NAME.setTooltip(FILE_NAME_TIP);
+        ASSEMBLY_NAME.setTooltip(ASSEMBLY_NAME_TIP);
+        ASSEMBLY_PATH.setTooltip(ASSEMBLY_PATH_TIP);
         /**
          * Define the look and behavior of the GridPane
          */
@@ -42,9 +58,9 @@ public class AssemblyGridPane extends GridPane {
         this.setVgap( 4 );
         //Define column behavior (min_size, preferred_size, max_size)
         ColumnConstraints c0 = new ColumnConstraints( 25, 25, 50 );
-        ColumnConstraints c1 = new ColumnConstraints( 25, 25, 50 );
-        ColumnConstraints c2 = new ColumnConstraints( 25, 150, 200 );
-        ColumnConstraints c3 = new ColumnConstraints( 25, 50, 150 );
+        ColumnConstraints c1 = new ColumnConstraints( 25, 100, 150 );
+        ColumnConstraints c2 = new ColumnConstraints( 25, 100, 150 );
+        ColumnConstraints c3 = new ColumnConstraints( 25, 50, 50 );
         //Define column auto-resizing behavior
         c1.setHgrow( Priority.NEVER );
         c2.setHgrow( Priority.ALWAYS );
@@ -57,26 +73,63 @@ public class AssemblyGridPane extends GridPane {
          * Define the look and behavior of the non-static TextField and Label elements
          */
 
-        // Add the sample nodes to to row 0 column 0 and 2
-        this.add( SAMPLE_NAME, 0, 0, 3, 1 );
-        this.add( sample_name, 0, 2, 3, 1);
+        // Add the title to row 0 column 0
+        this.add(ASSEMBLY, 0, 0, 3, 1 );
 
-        // Add file nodes to row 2 column 0 and 2
-        this.add( FILE_NAME, 1, 0, 3, 1 );
-        this.add( file_name, 1, 2, 3, 1 );
+        // Add row headings for app-path and app-args to column 1
+        this.add(ASSEMBLY_NAME, 1, 1, 3, 1 );
+        this.add(ASSEMBLY_PATH, 1, 2, 3, 1 );
+
+        this.add(assembly_name, 2, 1, 3, 1 );
+        this.add(assembly_path, 2, 2, 3, 1 );
+
+        ArrayList<TextField> temp = new ArrayList<>();
+        elements = FXCollections.observableArrayList(temp);
+
+        elements.addAll( assembly_name, assembly_path );
+
+        elements.addListener(new ListChangeListener<TextField>() {
+            @Override
+            public void onChanged(Change<? extends TextField> c) {
+                if( assembly == null ){
+                    assembly = new Assembly();
+                }
+                while( c.next() ){
+                    //Hacky, any change will save all fields to the xmlbind
+                    assembly.setSample( assembly_name.getText() );
+                    assembly.setValue( assembly_path.getText() );
+                }
+            }
+        });
     }
 
-    String getSampleName(){
-        return sample_name.getText();
+    String getAssemblyName(){
+        return assembly_name.getText();
     }
-    void setSampleName( String name ){
-        sample_name.setText( name );
+    void setAssemblyName( String name ){
+        assembly_name.setText( name );
     }
 
-    String getFileName(){
-        return file_name.getText();
+    String getAssembly(){
+        return assembly_path.getText();
     }
-    void setFileName( String name ){
-        file_name.setText( name );
+    void setAssembly( String file ){
+        assembly_path.setText( file );
+    }
+
+    @Override
+    void setTitle(String title) {
+
+    }
+
+    void clear() {
+        setAssemblyName( "" );
+        setAssembly( "" );
+    }
+
+    void setXMLBind( Assembly assembly_obj ){
+        assembly = assembly_obj;
+        assembly_name.setText( assembly_obj.getSample() );
+        assembly_path.setText( assembly_obj.getValue() );
     }
 }
