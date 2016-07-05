a6095f979e4fc2a2345bd85e505da497da4a29b3
JP Labadie <jlabadie@tgen.org>
2016-07-05 11:51:54 -0700

    Add XmlRootElement annotation

--- src/main/java/xmlbinds/NaspInputData.java
+++ src/main/java/xmlbinds/NaspInputData.java
@@ -9,10 +9,7 @@
 package xmlbinds;
 
 import javax.annotation.Generated;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 
 /**
@@ -37,6 +34,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
 @XmlType(name = "NaspInputData", propOrder = {
     "options",
     "files",
