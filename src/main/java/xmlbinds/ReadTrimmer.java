d0717496858a2d1223886d771ed2d9bdab2474f4
JP Labadie <jlabadie@tgen.org>
2016-07-19 09:49:41 -0700

    Add implements Application to ReadTrimmer

--- src/main/java/xmlbinds/ReadTrimmer.java
+++ src/main/java/xmlbinds/ReadTrimmer.java
@@ -37,7 +37,7 @@ import javax.xml.bind.annotation.XmlType;
     "jobParameters"
 })
 @Generated(value = "com.sun.tools.internal.xjc.Driver", date = "2016-07-19T09:20:06-07:00", comments = "JAXB RI v2.2.8-b130911.1802")
-public class ReadTrimmer {
+public class ReadTrimmer implements Application{
 
     @XmlElement(name = "AdditionalArguments", required = true)
     @Generated(value = "com.sun.tools.internal.xjc.Driver", date = "2016-07-19T09:20:06-07:00", comments = "JAXB RI v2.2.8-b130911.1802")
